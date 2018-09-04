a73fe077d544dd6a56a98a231ef658b46d7331aa
Trevahok <vighneshss@gmail.com>
2018-09-04 19:26:54 +0530

    before tkinter ui

--- amazon_scraper/copied_scraper.py
+++ amazon_scraper/copied_scraper.py
@@ -9,7 +9,6 @@ from bs4 import BeautifulSoup as bs
 
 def AmzonParser(url):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
-    # page = requests.get(url,headers=headers)
     try:
         req = urllib.request.Request(url,headers=headers)
         page = urlopen(req).read()
@@ -23,26 +22,17 @@ def AmzonParser(url):
             soup = bs(page)
             PRICES=soup.select_one('#soldByThirdParty > span').text.split()[-1]
             XPATH_NAME = '//h1[@id="title"]//text()'
-            # XPATH_SALE_PRICE = '//span[contains(@id,"ourprice") or contains(@id,"saleprice")]/text()'
-            # XPATH_SALE_PRICE='//*[@id="a-autoid-4-announce"]/span[2]/span/span'
-            # XPATH_ORIGINAL_PRICE = '//td[contains(text(),"List Price") or contains(text(),"M.R.P") or contains(text(),"Price")]/following-sibling::td/text()'
             XPATH_CATEGORY = '//a[@class="a-link-normal a-color-tertiary"]//text()'
             XPATH_AVAILABILITY = '//div[@id="availability"]//text()'
  
             RAW_NAME = doc.xpath(XPATH_NAME)
-            # RAW_SALE_PRICE = doc.xpath(XPATH_SALE_PRICE)
             RAW_CATEGORY = doc.xpath(XPATH_CATEGORY)
-            # RAW_ORIGINAL_PRICE = doc.xpath(XPATH_ORIGINAL_PRICE)
             RAw_AVAILABILITY = doc.xpath(XPATH_AVAILABILITY)
  
             NAME = ' '.join(''.join(RAW_NAME).split()) if RAW_NAME else None
-            # SALE_PRICE = ' '.join(''.join(RAW_SALE_PRICE).split()).strip() if RAW_SALE_PRICE else None
             CATEGORY = ' > '.join([i.strip() for i in RAW_CATEGORY]) if RAW_CATEGORY else None
-            # ORIGINAL_PRICE = ''.join(RAW_ORIGINAL_PRICE).strip() if RAW_ORIGINAL_PRICE else None
             AVAILABILITY = ''.join(RAw_AVAILABILITY).strip() if RAw_AVAILABILITY else None
  
-            # if not ORIGINAL_PRICE:
-                # ORIGINAL_PRICE = SALE_PRICE
  
             data = {
                     'NAME':NAME,
@@ -50,7 +40,7 @@ def AmzonParser(url):
                     'PRICES':PRICES,
                     'AVAILABILITY':AVAILABILITY,
                     'URL':url,
-                    'ASIN':url.split('\/')[-1],
+                    'ASIN':url.split('/')[-1],
                     }
  
             print(data)
@@ -74,5 +64,4 @@ def process_handler(AsinList):
         sleep(5)
 
 logging.basicConfig(filename='scraped_data.json',level=logging.INFO,format='%(message)s')
-logging.info('[')
 
--- amazon_scraper/new_scraper.py
+++ amazon_scraper/new_scraper.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.keys import Keys
 import copied_scraper  as scraper_util
 import logging
 
-web = webdriver.Chrome('/Users/ssvighnesh/chromedriver')
 
 def get_asins(words):
+    web = webdriver.Chrome('/Users/ssvighnesh/chromedriver')  #replace this with an absolute path of a local driver that is downloaded from selenium website
     keywords = '+'.join(words.strip().split())
     web.get('https://www.amazon.in/s?url=search-alias%3Daps&field-keywords=' + keywords)
     page= web.page_source
@@ -17,10 +17,11 @@ def get_asins(words):
     for thing in things:
         if thing.get('data-asin'):
             new.append(thing.get('data-asin'))
+    web.close()
     return new
 
-list_of_asins = get_asins('shiva trilogy')
-web.close()
+text_to_search = input('enter the term to search for:')
+list_of_asins = get_asins(text_to_search)
 logging.basicConfig(filename='scraped_data.json',level=logging.INFO,format='%(message)s')
 
 scraper_util.process_handler(list_of_asins)
