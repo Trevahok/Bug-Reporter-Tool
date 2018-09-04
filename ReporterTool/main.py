cac002d8ccacb1578292eece4311dfa0568e593c
Trevahok <vighneshss@gmail.com>
2018-09-04 13:19:34 +0530

    working on regex

--- ReporterTool/main.py
+++ ReporterTool/main.py
@@ -1,7 +1,8 @@
-from FileManager import *
+import FileManager
+import re 
 
-f = WordFileManager(r'C:\Users\Sainath\Documents\Codeathon\CODE-A-THON-readme-master\CODE-A-THON-readme-master\Question-5\Source and report\Developer Report\Test_DeveloperReview_with Bug.docx')
-f2 = FileManager(r'C:\Users\Sainath\Desktop\bleh.txt')
-print(f.readCells())
-wordstring = f2.readFile()
-print(wordstring)
+word_file =FileManager.WordFileManager('./Developer Report/Test_DeveloperReview_with Bug.docx')
+
+source = FileManager.FileManager('./Source Code/Original_sample_code.c')
+text = re.findall(r'\/\*.*\*\/', source )
+print(text)
