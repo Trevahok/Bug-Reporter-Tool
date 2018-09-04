cbffaaf215e8640087fd578398883d6be5a884ab
Trevahok <vighneshss@gmail.com>
2018-09-04 20:23:14 +0530

    commandline util scraper complete

--- amazon_scraper/new_scraper.py
+++ amazon_scraper/new_scraper.py
@@ -4,9 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import copied_scraper  as scraper_util
 import logging
+import matplotlib.pyplot as plt
+import json
+from sys import argv
 
+logging.basicConfig(filename='scraped_data.json',level=logging.INFO,format='%(message)s')
 
-def get_asins(words):
+def get_asins():
+    words = input('enter the term to search for:')
     web = webdriver.Chrome('/Users/ssvighnesh/chromedriver')  #replace this with an absolute path of a local driver that is downloaded from selenium website
     keywords = '+'.join(words.strip().split())
     web.get('https://www.amazon.in/s?url=search-alias%3Daps&field-keywords=' + keywords)
@@ -20,9 +25,29 @@ def get_asins(words):
     web.close()
     return new
 
-text_to_search = input('enter the term to search for:')
-list_of_asins = get_asins(text_to_search)
-logging.basicConfig(filename='scraped_data.json',level=logging.INFO,format='%(message)s')
 
-scraper_util.process_handler(list_of_asins)
+def plot_this():
+    with open('scraped_data.json') as df:
+        data = '[' + df.read()[:-2] +']'
+        data =  json.loads(data)
 
+    data.sort(key = lambda i :float( i['PRICES']))
+    names=[i['NAME'] for i in data ] 
+    prices = [i['PRICES'] for i in data]
+    plt.plot(names,prices)
+    plt.show()
+
+try:
+    mode = argv[1]
+except:
+    print('Usage :\n\n python3 new_scraper.py gen_dat \n\n or \n\n python3 new_scraper.py plot_dat \n\n ')
+    exit()
+
+if mode == 'plot_dat':
+    plot_this()
+elif mode=='gen_dat':
+    list_of_asins = get_asins()
+    scraper_util.process_handler(list_of_asins)
+else:
+    print('usage : python3 new_scraper.py gen_dat \n\n or \n\n python3 new_scraper.py plot_dat')
+    exit()
